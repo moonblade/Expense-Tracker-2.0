fbef59fe69c1909052592e0b0468876b316211b0
moonblade <moonblade168@gmail.com>
2025-01-04 00:02:48 +0530

    git crypt test

--- functions/getToken/functions/utils/token.py
+++ functions/getToken/functions/utils/token.py
@@ -1,8 +1,21 @@
 from firebase_functions import https_fn
+import requests
 
-def get_token_from_creds(req: https_fn.Request) -> https_fn.Response:
+def get_token_from_creds(email, password):
+
+    pass
+
+def get_token_from_req(req: https_fn.Request) -> https_fn.Response:
     if req.method != "POST":
         return https_fn.Response("Invalid request method. Please use POST."), 405
 
-    token = "hello world"
+    request_data = req.get_json()
+    email = request_data.get("email")
+    password = request_data.get("password")
+
+    if not email or not password:
+        return https_fn.Response("Email and password are required."), 400
+
+    token = get_token_from_creds(email, password)
+
     return https_fn.Response(token)
