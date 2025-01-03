961bb2a7daaa209041c8a9f3519308ad883bb7c0
moonblade <moonblade168@gmail.com>
2025-01-03 16:59:27 +0530

    Post get token method

--- functions/getToken/functions/main.py
+++ functions/getToken/functions/main.py
@@ -4,10 +4,10 @@
 
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
+from utils.token import get_token_from_creds
 
 initialize_app()
 
-
 @https_fn.on_request()
-def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    return https_fn.Response("Hello world!")
+def get_token(req: https_fn.Request) -> https_fn.Response:
+    return get_token_from_creds(req)
