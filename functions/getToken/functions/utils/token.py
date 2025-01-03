af442c2f8077225cd905d1aebd34ed3b5c118fec
moonblade <moonblade168@gmail.com>
2025-01-04 01:10:49 +0530

    Allow saving sms with api

--- functions/getToken/functions/utils/token.py
+++ functions/getToken/functions/utils/token.py
@@ -1,9 +1,18 @@
 from firebase_functions import https_fn
 import requests
 
-def get_token_from_creds(email, password):
+with open("./secrets/firebaseapikey", "r") as f:
+    FIREBASE_API_KEY = f.read().strip()
 
-    pass
+def get_token_from_creds(email, password):
+    response = requests.post(f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={FIREBASE_API_KEY}", json={
+        "email": email,
+        "password": password,
+        "returnSecureToken": True
+    })
+    if response.status_code != 200:
+        return False, "Invalid credentials"
+    return True, response.json().get("idToken")
 
 def get_token_from_req(req: https_fn.Request) -> https_fn.Response:
     if req.method != "POST":
@@ -16,6 +25,8 @@ def get_token_from_req(req: https_fn.Request) -> https_fn.Response:
     if not email or not password:
         return https_fn.Response("Email and password are required."), 400
 
-    token = get_token_from_creds(email, password)
+    success, token = get_token_from_creds(email, password)
+    if not success:
+        return https_fn.Response(token), 401
 
-    return https_fn.Response(token)
+    return https_fn.Response(token), 200
