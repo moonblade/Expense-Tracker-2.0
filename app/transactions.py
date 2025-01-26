d4ad064cb2281cf733120ac643d535b5f02d72e2
moonblade <moonblade168@gmail.com>
2025-01-26 20:39:49 +0530

    Add category if merchant has been seen before

--- app/transactions.py
+++ app/transactions.py
@@ -1,5 +1,5 @@
 from typing import List
-from db import get_transaction, update_transaction
+from db import add_merchant, add_transactions_db, get_merchants, get_transaction, update_transaction
 from models import Category, Transaction
 from fastapi import HTTPException
 
@@ -24,5 +24,33 @@ def categorize_transaction(transaction_id: str, category: Category, email: str) 
     if not transaction:
         raise HTTPException(status_code=400, detail="Transaction not found")
     transaction.category = category
+    add_merchant(transaction.merchant, category)
     update_transaction(email, transaction.id, transaction)
     return "ok"
+
+def add_transactions(email: str, transactions: List[Transaction]):
+    if not transactions:
+        return False
+    print(f"Adding {len(transactions)} transactions")
+
+    transactionToAdd = []
+    for transaction in transactions:
+        existingtransaction = get_transaction(email, transaction.id)
+        if existingtransaction:
+            if existingtransaction.category == Category.uncategorized:
+                merchants = get_merchants()
+                if existingtransaction.merchant in merchants:
+                    category = Category(merchants[existingtransaction.merchant]["category"])
+                    existingtransaction.category = category
+                    print(f"Updating category for {existingtransaction.merchant} to {category}")
+                    update_transaction(email, existingtransaction.id, existingtransaction)
+        else:
+            if transaction.category == Category.uncategorized:
+                merchants = get_merchants()
+                if transaction.merchant in merchants:
+                    category = Category(merchants[transaction.merchant]["category"])
+                    print(f"Updating category for {transaction.merchant} to {category}")
+                    transaction.category = category
+            transactionToAdd.append(transaction)
+
+    add_transactions_db(email, transactionToAdd)
